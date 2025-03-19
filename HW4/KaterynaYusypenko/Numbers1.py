2f993fc63781ad5080a27dd41e497a58abd7f045
KateYusypenko <ikat.bondarenko@gmail.com>
2025-03-19 15:13:34 +0100

    Updated Numbers1.py and removed .DS_Store

--- HW4/KaterynaYusypenko/Numbers1.py
+++ HW4/KaterynaYusypenko/Numbers1.py
@@ -2,7 +2,7 @@ number1= int(input("Enter a number1: "))
 number2= int(input("Enter a number2: "))
 if number1 > number2:
     print(f"{number1} is bigger then {number2}.")
-elif number2>number1:
+elif number2>number1:git
     print(f"{number2} is bigger then {number1}.")
 else:
     print("Both numbers are equal")
