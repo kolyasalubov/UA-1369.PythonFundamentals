3cbda5a9e6b1e5c949630600e42340d514bd39eb
Rostyslav Kaziuta <rostykkaziuta@gmail.com>
2025-03-25 12:51:02 +0200

    add hw5, hw6

---
+++ HW5/KaziutaRostyslav/hw5.py
@@ -0,0 +1,41 @@
+# list_num = [1,2,3,4,5,6,7,8,"9","10"]
+# float_list = []
+# for i in list_num:
+#     if isinstance(i,int):
+#         float_list.append(float(i))
+#
+# print(float_list)
+
+
+
+# x = int(input("Enter a number: "))
+# a, b = 0, 1
+# for i in range(1, x+1):
+#     print("{} => {}"format(i, a))
+#     a, b = b, a+b
+
+# a, b = 0, 1
+# i = 1
+# while a <= 13:
+#     print("{} => {}".format(i, a))
+#     a, b = b, a + b
+#     i += 1
+
+
+
+# a = int(input("Factorial of: "))
+# factorial = 1
+# for i in range(1,a+1):
+#     factorial = factorial * i
+#
+# print(factorial)
+
+
+
+
+
+
+
+
+
+
