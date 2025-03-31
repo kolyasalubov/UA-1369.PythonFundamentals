dc72659c28d271bd310acdf4d1fb9e477ef005b4
KateYusypenko <ikat.bondarenko@gmail.com>
2025-03-31 20:11:38 +0200

    Modifided Task1

--- HW6/KaterynaYusypenko/Task1.py
+++ HW6/KaterynaYusypenko/Task1.py
@@ -5,11 +5,11 @@ other_numbers = []
 for item in range(1, 11):
     if item % 2 == 0:
         even_numbers.append(item)
-    elif item % 3 == 0:
+    if item % 3 == 0:
         odd_numbers_divisible_by_3.append(item)
-    else:
+    if item % 2 != 0 and item % 3 != 0:
         other_numbers.append(item)
 
 print(f"Even numbers: {even_numbers}")
 print(f"Odd numbers divisible by 3: {odd_numbers_divisible_by_3}")
-print(f"Other numbers (not divisible by 2 and 3): {other_numbers}")
+print(f"Other numbers: {other_numbers}")
