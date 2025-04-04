0cc9a5efa30effd8b4598c83af34d99dd10127ae
Yuliia Ihnat <jgromova912@gmail.com>
2025-04-04 16:13:56 +0300

    added

--- HW3/YuliiaIhnat/03.1_Practical_tasks.py
+++ HW3/YuliiaIhnat/03.1_Practical_tasks.py
@@ -1,5 +1,31 @@
 # Task №1
+zen = """The Zen of Python, by Tim Peters
 
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+
+print("Better in zen - ", zen.find("better"))
+print("Never in zen - ", zen.find("never"))
+print("is in zen - ", zen.find("is"))
+print("Zen in uppercase -", zen.upper())
+print("Replace 'i' with '&' in zen - ", zen.replace("i", "&"))
 
 # Task №2
 a = 2850
