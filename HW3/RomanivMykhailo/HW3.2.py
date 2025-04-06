780cc4fd5e940f1525736dd8bc72722304cef269
“Mykhailo <misharomaniv637@gmail.com>
2025-04-06 18:50:01 +0300

    added HW3

--- HW3/RomanivMykhailo/HW3.2.py
+++ HW3/RomanivMykhailo/HW3.2.py
@@ -1,1 +1,22 @@
-print('Hello world')
+#знайти кількість цифр в числі та порахувати їх добуток
+number = 8214
+digits = [int(a) for a in str(number)]
+
+# Кількість цифр
+count = len(digits)
+
+# Добуток цифр
+product = 1
+for digit in digits:
+    product *= digit
+
+# Цифри написані у зворотному порядку
+reversed_digits = digits[::-1]
+
+# Відсортовані цифри по зростанню
+sorted_digits = sorted(digits)
+
+print("Кількість цифр:", count)
+print("Добуток цифр:", product)
+print("Цифри в зворотному порядку:", reversed_digits)
+print("Цифри відсортовані по зростанню:", sorted_digits)
