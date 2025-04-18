8c1fbd1f65269528ce4809227cc097eeacdc1913
Yuliia Ihnat <jgromova912@gmail.com>
2025-04-18 15:18:31 +0300

    Yuliia_Ihnat_addet_HW4

---
+++ HW4/YuliiaIhnat/Temperature.py
@@ -0,0 +1,8 @@
+celsius = float(input("Enter the temperature in Celsius: "))
+F = (celsius * 9 / 5) + 32
+if celsius <= -273.15:
+    print("Eror: Temperature below absolute zero (-273.1°C)")
+elif celsius > -273.15:
+    print(celsius, "°C is equivalent to", F, "°F")
+else:
+    print("Syntaxis eror, please enter temperature again")
